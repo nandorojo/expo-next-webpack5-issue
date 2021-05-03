35a40683b214a4f4d0bd58681b96edc60944d1d6
Fernando Rojo <frojo@sas.upenn.edu>
2021-05-02 21:51:47 -0400

    works without RNW imported

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const { withExpo } = require("@expo/next-adapter");
 const withPlugins = require("next-compose-plugins");
 
+const withTM = require("next-transpile-modules")(["react-native-web"]);
+
 module.exports = withPlugins(
   [
+    withTM,
     [
       withExpo,
       {
